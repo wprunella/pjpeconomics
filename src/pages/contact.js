0c86fbb925b4569d4e2b0dcd28dbdee6c3de4e85
Warren Prunella <wprunella@lionfiregroup.com>
2022-02-28 11:34:58 -0500

    add email button to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -17,6 +17,14 @@ export default function Contact() {
         <p>
           <strong>Phone: </strong>301-231-7944
         </p>
+        <p>
+          <a
+            href="mailto: %69%6E%66%6F%40%70%6A%70%65%63%6F%6E%6F%6D%69%63%73%2E%63%6F%6D"
+            className="btn btn-success"
+          >
+            Email Us
+          </a>
+        </p>
       </div>
     </Layout>
   )
